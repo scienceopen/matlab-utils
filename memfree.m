0774d7238cb1fe2b4115195c838b5d302100b2f0
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-09-24 17:20:34 -0400

    modernize psutil

--- memfree.m
+++ memfree.m
@@ -12,57 +12,25 @@
 % --------
 % returns estimate of free physical RAM in bytes
 %
-% Michael Hirsch 2012
+% Michael Hirsch, Ph.D.
 
 function freebytes = memfree()
 
 try
+    freebytes = double(py.psutil.virtual_memory().available);
+    meth='py.psutil';
+catch
     [err,freebytes]=system('python -c "import psutil; print(psutil.virtual_memory().available)"');
     if ~err
         freebytes=str2double(freebytes);
-        disp([num2str(freebytes/1e9,'%0.2f'),' Gbyte available RAM via python psutil'])
+        meth='python psutil';
+    else
+        error('Python psutil not found.')
     end
-catch
-    if ispc  % for Cygwin, isunix=true && ispc=false
-        freebytes = memorywindows();
-    elseif ismac % BSD
-        freebytes = memorymac(); % we did not handle Macs yet (request if you want)
-    else %isunix && ~ismac || iscygwin
-        freebytes = memoryunix();
-    end %if
-    disp([num2str(freebytes/1e9,'%0.2f'),' Gbyte available RAM via Matlab fallback'])
-end %try
-
-
-if ~nargout,clear,end
-end %function
-
-function freebytes = memoryunix()
-%% free + buffers + cached
-[~,msg] = unix('free -mb | awk "NR==2"');
-
-mems = cell2mat(textscan(msg,'%*s %f %f %f %f %f %f','delimiter',' ','collectoutput',true,'multipleDelimsAsOne',true));
-
-freebytes = mems(1,3)+mems(1,5)+mems(1,6);
-
-end %function
-
-function freebytes = memorymac()
-%% inactive + free
-% TODO: looks like we could awk vm_stat to get it
-%  "Pages inactive"   "Pages free"
-freebytes=[];
-
-end %function
-
-function freebytes = memorywindows()
-
-if isoctave % is windows (and not on Cygwin)
-    [~,msg] = system('wmic OS get FreePhysicalMemory /Value');
-    freebytes = str2double(msg(26:end))*1e3;
-else % is matlab
-    [~,s]=memory;
-    freebytes = s.PhysicalMemory.Available;
 end
 
+%%
+disp([num2str(freebytes/1e9,'%0.2f'),' Gbyte available RAM via ',meth])
+
+if ~nargout,clear,end
 end %function
