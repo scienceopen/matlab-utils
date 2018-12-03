70b5a182de37a332ac65855ce2978ce43f777ffb
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-12-03 15:02:37 -0500

    init

---
+++ find_fortran_compiler.m
@@ -0,0 +1,51 @@
+function fc = find_fortran_compiler(FC)
+%% Find Fortran compiler
+% intended for Fortran 2008+ compilers
+%
+% fc = find_fortran_compiler()   finds the first Fortran compiler available
+% and working on your system.
+%
+% fc = find_fortran_compiler(FC) searches first for the compiler specified
+% in string FC.
+%
+%  Michael Hirsch, Ph.D.
+
+% stops upon finding first working Fortran compiler
+FCs = {'gfortran', 'ifort', 'flang', 'pgf95', 'nagfor', ...
+       'ftn', ... % Cray
+       'xlf2008', ... % IBM XL
+       'f95', ... % Absoft
+       };
+
+if nargin==0
+  FC = getenv('FC');
+else
+  validateattributes(FC,{'char'},{'vector'})
+end
+
+if ~isempty(FC)
+    FCs = [{FC},FCs];
+end
+
+fstem = tempname;
+fn = [fstem, '.f90'];
+rfn = [fstem, '.exe'];
+
+prog = 'program a; use iso_fortran_env; print *,compiler_version(); end';
+fid = fopen(fn, 'wt');
+fprintf(fid, '%s\n', prog);
+fclose(fid);
+
+for i = 1:length(FCs)
+  fc = FCs{i};
+  [stat, ~] = system([fc, ' ', fn, ' -o',rfn]);  % gobble error messages
+  if stat ~= 0, continue, end
+  
+  [stat, msg] = system(rfn);
+  if stat == 0
+      disp(msg)
+      return
+  end
+end
+
+end
