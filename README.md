cfd1fb4a1d122787cd49af1d63b7e49a589483e8
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-17 18:07:36 -0500

    absolute_path handle more cases

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 
-[![Actions Status](https://github.com/scivision/matlab-octave-utils/workflows/ci/badge.svg)](https://github.com/scivision/matlab-octave-utils/actions)
-
-
 # Matlab and GNU Octave Utilities
+
+[![Actions Status](https://github.com/scivision/matlab-octave-utils/workflows/ci/badge.svg)](https://github.com/scivision/matlab-octave-utils/actions)
 
 Utilities for Matlab/Octave:
 
+* absolute_path.m: determine absolute path on Octave & Matlab OS-independent
 * `is*.m`: OS detection
 * `memfree.m`: free memory estimation
 * `diskfree.m`: disk free space
--- absolute_path.m
+++ absolute_path.m
@@ -2,15 +2,17 @@
 function abspath = absolute_path(path)
 % path need not exist, but absolute path is returned
 
+narginchk(1,1)
 % have to expand ~ first
 path = expanduser(path);
 
 if isoctave
   abspath = make_absolute_filename(path);
 else
-  if path(1) == '.'
+  if path(1) == '.' || ...
+     ~contains(path, filesep) || ...
+     (isletter(path(1)) && ~strcmp(path(2), ':'))
     % TODO: handle .file or .path/.file
-    % this is necessary when run from arbitrary directories.
     path = [pwd, filesep, path];
   end
   abspath = char(java.io.File(path).getCanonicalPath());
