7bf98f13dbed5b7a89e6d8cb6e12f4810ae5ee09
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-17 18:06:13 -0500

    memfree -1 if not findable

--- memfree.m
+++ memfree.m
@@ -1,12 +1,26 @@
 function freebytes = memfree()
 %% find free physical RAM on Windows (with or without Cygwin) and Linux systems
-% Matlab at least through R2019a doesn't support memory() on Linux / Mac systems
+% Matlab doesn't support memory() on Linux / Mac systems
 % This function gives free memory on Windows, MacOS and Linux using Python psutil.
 %
 % Output:
 % --------
 % free physical RAM [bytes]
+%
+% Returns -1 if memory is not determinable.
 
-freebytes = int64(py.psutil.virtual_memory().available);
+freebytes = -1;
+
+if isoctave
+  [status, msg] = system('python3 -c "import psutil; print(psutil.virtual_memory().available)"');
+  if status == 0
+    freebytes = int64(msg);
+  endif
+else
+  try
+    freebytes = int64(py.psutil.virtual_memory().available);
+  end
+endif
+
 
 end %function
