9fc05f046b64cff28f3bb64bf24795ee0af3b9aa
Michael Hirsch <scivision@users.noreply.github.com>
2020-05-06 09:03:34 -0400

    more robust absolute path

--- absolute_path.m
+++ absolute_path.m
@@ -1,6 +1,9 @@
 %!assert(ischar(absolute_path('~')))
 function abspath = absolute_path(path)
 % path need not exist, but absolute path is returned
+%
+% NOTE: GNU Octave is weaker at this, especially if /foo/bar/../baz and "bar"
+% doesn't exist, it may just return the input unmodified.
 
 narginchk(1,1)
 % have to expand ~ first
@@ -9,13 +12,18 @@ path = expanduser(path);
 if isoctave
   abspath = make_absolute_filename(path);
 else
-  if path(1) == '.' || ...
-     ~contains(path, filesep) || ...
-     (isletter(path(1)) && ~strcmp(path(2), ':'))
+  if is_relative_path(path)
     % TODO: handle .file or .path/.file
-    path = [pwd, filesep, path];
+    path = fulllfile(pwd, path);
   end
-  abspath = char(java.io.File(path).getCanonicalPath());
+  try
+    abspath = char(java.io.File(path).getCanonicalPath());
+  catch exc
+    error('absolute_path:value_error', 'could not make absolute path from %s', path)
+  end
 end
+
+% debugging
+% disp([path, ' => ',abspath])
 
 end % function
