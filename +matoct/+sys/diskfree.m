a881c8648c76dfd2457bc8743c065a8ec362b024
Michael Hirsch <scivision@users.noreply.github.com>
2020-08-18 09:15:57 -0400

    octave workaround

--- +matoct/+sys/diskfree.m
+++ +matoct/+sys/diskfree.m
@@ -5,6 +5,11 @@ function freebytes = diskfree(mydir)
 narginchk(1,1)
 validateattributes(mydir, {'char'}, {'vector'}, mfilename, 'drive letter to get free space from', 1)
 
+if matoct.sys.isoctave
+    freebytes = -1;
+    return
+end
+
 mydir = matoct.fileio.expanduser(mydir);
 
 assert(isfolder(mydir), '%s is not a folder', mydir)
